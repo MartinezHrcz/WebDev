4f1d36371b180746d8b78b99d550dd6ecfe3133f
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-03-05 19:17:58 +0100

    feat: trying out javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function spellout(){
+    document.getElementById("datehere").innerHTML = "sldfljdslk";
+}
+
+function AddNew(){
+    var str = document.getElementById("inputtxt").value;
+    document.getElementById("tasks").innerHTML += str;
+}
